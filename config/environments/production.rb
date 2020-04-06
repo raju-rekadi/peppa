98573e13898c37f39faa3a2d0367df5760035e0f
Raju <rajurekadi7@gmail.com>
2020-04-06 19:58:40 +0530

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
 
-# config.serve_static_assets = true
+config.serve_static_assets = true
 # config.assets.compile = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
